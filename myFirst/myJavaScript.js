bbd4dc274a08870753e24814514f8e2637dfdb7f
liangjiaze <renmuda@163.com>
2021-01-21 16:22:13 +0800

    create web repository

---
+++ myFirst/myJavaScript.js
@@ -0,0 +1,14 @@
+// alert("你好啊!")
+// var fter ='100'
+// alert(fter)
+// alert(typeof fter)
+// bos = Number(fter)
+// alert(bos)
+// alert(typeof bos)
+// var a = 100
+// var b = "null"
+// var c = Number(false)
+// alert(c)
+window.onload = function att() {
+    alert("a")
+}
